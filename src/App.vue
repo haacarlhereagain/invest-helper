68d2470edbd81e2bc37670d45be144c5ff78f42d
Alexandr Khalivetov <haacarlhereagain@gmail.com>
2024-08-30 03:04:12 +0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <input v-model="sum" type="number">
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const sum = ref<number>();
+const apr = ref<number>();
+
+const templateIds = {
+  sum: crypto.randomUUID(),
+  apr: crypto.randomUUID(),
+}
+
+</script>
+
+<style scoped>
+
+</style>
